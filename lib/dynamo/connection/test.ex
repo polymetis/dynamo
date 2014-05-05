2ba210eab06808495c402459f420975813e24655
matt <mgwidmann@gmail.com>
2014-05-04 21:30:10 -0400

    Revert "Push current code to try to get help..."
    This reverts commit b8f8353f257064ae630d1c9fd5754290fb5eb21c.

--- lib/dynamo/connection/test.ex
+++ lib/dynamo/connection/test.ex
@@ -4,11 +4,11 @@ defmodule Dynamo.Connection.Test do
   Check `Dynamo.Connection` for documentation of all available callbacks.
 
   Note that, besides the functions required by `Dynamo.Connection`,
-  this Connection also implements a couple extra functions, like
+  this connection also implements a couple extra functions, like
   `sent_body/1`, to retrieve the sent body, and `fetched/1`, to
   retrieve fetched aspects, which are useful for testing.
 
-  Although a new Connection can be created via:
+  Although a new connection can be created via:
 
       Dynamo.Connection.Test.new(verb, path, body, peer)
 
@@ -25,16 +25,16 @@ defmodule Dynamo.Connection.Test do
       :path, :path_segments, :sent_body, :original_method, :scheme, :port, :peer ]
 
   @doc """
-  Initializes a Connection to be used in tests.
+  Initializes a connection to be used in tests.
   """
   def new(method, path, body \\ "") do
-    %Connection{
+    connection(
       main: Dynamo.under_test,
       raw_req_cookies: Binary.Dict.new(),
       raw_req_headers: Binary.Dict.new([{ "host", "127.0.0.1" }]),
       scheme: :http,
       port: 80
-    }.recycle.req(method, path, body)
+    ).recycle.req(method, path, body)
   end
 
   ## Request API
@@ -45,51 +45,51 @@ defmodule Dynamo.Connection.Test do
   end
 
   @doc false
-  def original_method(%Connection{original_method: method}) do
+  def original_method(connection(original_method: method)) do
     method
   end
 
   @doc false
-  def query_string(%Connection{query_string: query_string}) do
+  def query_string(connection(query_string: query_string)) do
     query_string
   end
 
   @doc false
-  def path_segments(%Connection{path_segments: path_segments}) do
+  def path_segments(connection(path_segments: path_segments)) do
     path_segments
   end
 
   @doc false
-  def path(%Connection{path: path}) do
+  def path(connection(path: path)) do
     path
   end
 
   @doc false
-  def scheme(%Connection{scheme: scheme}) do
+  def scheme(connection(scheme: scheme)) do
     scheme
   end
 
   @doc false
-  def port(%Connection{port: port}) do
+  def port(connection(port: port)) do
     port
   end
 
   @doc false
-  def peer(%Connection{peer: peer}) do
+  def peer(connection(peer: peer)) do
     peer
   end
 
   def peer(peer, conn) do
-    %Connection{conn | peer: peer}
+    connection(conn, peer: peer)
   end
 
   @doc false
-  def host(%Connection{raw_req_headers: headers}) do
+  def host(connection(raw_req_headers: headers)) do
     hd(:binary.split(headers["host"], ":"))
   end
 
   @doc false
-  def host_url(%Connection{scheme: scheme, raw_req_headers: headers}) do
+  def host_url(connection(scheme: scheme, raw_req_headers: headers)) do
     "#{scheme}://#{headers["host"]}"
   end
 
@@ -109,54 +109,54 @@ defmodule Dynamo.Connection.Test do
   end
 
   @doc false
-  def send(status, body, %Connection{state: state} = conn) when is_integer(status)
+  def send(status, body, connection(state: state) = conn) when is_integer(status)
       and state in [:unset, :set] and is_binary(body) do
     send self, @send_flag
 
-    conn = run_before_send(%Connection{conn | state: :set, status: status, resp_body: body})
-    %Connection{resp_body: body} = conn
+    conn = run_before_send(connection(conn, state: :set, status: status, resp_body: body))
+    connection(resp_body: body) = conn
 
-    %Connection{conn |
+    connection(conn,
       state: :sent,
       sent_body: check_sent_body(conn, body),
       resp_body: nil
-    }
+    )
   end
 
   @doc false
-  def send_chunked(status, %Connection{state: state} = conn) when is_integer(status)
+  def send_chunked(status, connection(state: state) = conn) when is_integer(status)
       and state in [:unset, :set] do
     send self, @send_flag
-    conn = run_before_send(%Connection{conn | state: :chunked, status: status})
+    conn = run_before_send(connection(conn, state: :chunked, status: status))
 
-    %Connection{conn |
+    connection(conn,
       sent_body: "",
       resp_body: nil
-    }
+    )
   end
 
   @doc false
-  def chunk(body, %Connection{state: state, sent_body: sent} = conn) when state == :chunked do
-    { :ok, %Connection{conn, sent_body: check_sent_body(conn, sent <> body)} }
+  def chunk(body, connection(state: state, sent_body: sent) = conn) when state == :chunked do
+    { :ok, connection(conn, sent_body: check_sent_body(conn, sent <> body)) }
   end
 
-  defp check_sent_body(%Connection{original_method: "HEAD"}, _body), do: ""
+  defp check_sent_body(connection(original_method: "HEAD"), _body), do: ""
   defp check_sent_body(_conn, body),                                do: body
 
   @doc false
   def sendfile(status, path, conn) when is_integer(status) and is_binary(path) do
     send self, @send_flag
-    conn = run_before_send(%Connection{conn, state: :sendfile, status: status})
+    conn = run_before_send(connection(conn, state: :sendfile, status: status))
 
-    %Connection{conn |
+    connection(conn,
       state: :sent,
       sent_body: check_sent_body(conn, File.read!(path)),
       resp_body: nil
-    }
+    )
   end
 
   @doc false
-  def sent_body(%Connection{sent_body: sent_body}) do
+  def sent_body(connection(sent_body: sent_body)) do
     sent_body
   end
 
@@ -167,35 +167,34 @@ defmodule Dynamo.Connection.Test do
     Enum.reduce list, conn, fn(item, acc) -> acc.fetch(item) end
   end
 
-  def fetch(:headers, %Connection{raw_req_headers: raw_req_headers, req_headers: nil, fetched: fetched} = conn) do
-    %Connection{conn |
+  def fetch(:headers, connection(raw_req_headers: raw_req_headers, req_headers: nil, fetched: fetched) = conn) do
+    connection(conn,
       fetched: [:headers|fetched],
-      req_headers: raw_req_headers
-    }
+      req_headers: raw_req_headers)
   end
 
-  def fetch(:params, %Connection{query_string: query_string, params: nil, route_params: route_params, fetched: fetched} = conn) do
+  def fetch(:params, connection(query_string: query_string, params: nil, route_params: route_params, fetched: fetched) = conn) do
     params = Dynamo.Connection.QueryParser.parse(query_string)
     params = Dict.merge(params, route_params)
-    %Connection{conn | params: params, fetched: [:params|fetched]}
+    connection(conn, params: params, fetched: [:params|fetched])
   end
 
-  def fetch(:cookies, %Connection{raw_req_cookies: raw_req_cookies, req_cookies: nil, fetched: fetched} = conn) do
-    %Connection{conn | req_cookies: raw_req_cookies, fetched: [:cookies|fetched]}
+  def fetch(:cookies, connection(raw_req_cookies: raw_req_cookies, req_cookies: nil, fetched: fetched) = conn) do
+    connection(conn, req_cookies: raw_req_cookies, fetched: [:cookies|fetched])
   end
 
-  def fetch(:body, %Connection{raw_req_body: req_body, req_body: nil, fetched: fetched} = conn) do
-    %Connection{conn | req_body: req_body, fetched: [:body|fetched]}
+  def fetch(:body, connection(raw_req_body: req_body, req_body: nil, fetched: fetched) = conn) do
+    connection(conn, req_body: req_body, fetched: [:body|fetched])
   end
 
   def fetch(aspect, conn) when aspect in [:params, :cookies, :body, :headers] do
     conn
   end
 
-  def fetch(aspect, %Connection{fetchable: fetchable, fetched: fetched} = conn) when is_atom(aspect) do
+  def fetch(aspect, connection(fetchable: fetchable, fetched: fetched) = conn) when is_atom(aspect) do
     case Keyword.get(fetchable, aspect) do
       nil -> raise Dynamo.Connection.UnknownFetchError, aspect: aspect
-      fun -> %Connection{fun.(conn) | fetched: [aspect|fetched]}
+      fun -> connection(fun.(conn), fetched: [aspect|fetched])
     end
   end
 
@@ -205,11 +204,11 @@ defmodule Dynamo.Connection.Test do
   Sets the main module under test.
   """
   def main(main, conn) do
-    %Connection{conn | main: main}
+    connection(conn, main: main)
   end
 
   @doc """
-  Prepares the Connection to do a new request on the
+  Prepares the connection to do a new request on the
   given `path` with the given `method` and `body`.
 
   This can be considered the counter-part of recycle
@@ -223,7 +222,7 @@ defmodule Dynamo.Connection.Test do
     # Clear up any existing send flag
     flush_send
 
-    conn = %Connection{conn |
+    conn = connection(conn,
       method: method,
       original_method: method,
       params: nil,
@@ -234,19 +233,18 @@ defmodule Dynamo.Connection.Test do
       raw_req_body: body,
       req_body: nil,
       route_params: [],
-      script_name_segments: []
-    }
+      script_name_segments: [])
 
     if uri.authority do
       conn = conn.put_req_header "host", uri.authority
     end
 
     if uri.scheme do
-      conn = %Connection{conn | scheme: uri.scheme}
+      conn = connection(conn, scheme: uri.scheme)
     end
 
     if uri.port do
-      conn = %Connection{conn | port: uri.port}
+      conn = connection(conn, port: uri.port)
     end
 
     conn
@@ -261,16 +259,16 @@ defmodule Dynamo.Connection.Test do
   end
 
   @doc """
-  Recycles the Connection to it can be used in a subsequent requests.
+  Recycles the connection to it can be used in a subsequent requests.
 
-  Reclycing the Connection resets all assigns, privates and other
+  Reclycing the connection resets all assigns, privates and other
   response information but keeps mostly of request information intact.
 
   Particularly, cookies sent in the response are moved to the request,
   so they can be used in upcoming requests.
   """
-  def recycle(%Connection{resp_cookies: resp_cookies} = conn) do
-    conn = %Connection{conn |
+  def recycle(connection(resp_cookies: resp_cookies) = conn) do
+    conn = connection(conn,
       assigns: [],
       before_send: Dynamo.Connection.default_before_send,
       fetchable: [],
@@ -286,7 +284,7 @@ defmodule Dynamo.Connection.Test do
       sent_body: nil,
       state: :unset,
       status: nil
-    }
+    )
 
     Enum.reduce resp_cookies, conn, fn({ key, value, _opts }, acc) ->
       acc.put_req_cookie(key, value)
@@ -296,30 +294,30 @@ defmodule Dynamo.Connection.Test do
   @doc """
   Returns all fetched aspects during a request.
   """
-  def fetched(%Connection{fetched: fetched}) do
+  def fetched(connection(fetched: fetched)) do
     fetched
   end
 
   @doc """
   Sets the cookies to be read by the request.
   """
-  def put_req_cookie(key, value, %Connection{raw_req_cookies: cookies} = conn) do
-    %Connection{conn | raw_req_cookies: Dict.put(cookies, key, value)}
+  def put_req_cookie(key, value, connection(raw_req_cookies: cookies) = conn) do
+    connection(conn, raw_req_cookies: Dict.put(cookies, key, value))
   end
 
   @doc """
   Sets a request header, overriding any previous value.
   Both `key` and `value` are converted to binary.
   """
-  def put_req_header(key, value, %Connection{raw_req_headers: raw_req_headers} = conn) do
-    %Connection{conn | raw_req_headers: Dict.put(raw_req_headers, String.downcase(key), to_string(value))}
+  def put_req_header(key, value, connection(raw_req_headers: raw_req_headers) = conn) do
+    connection(conn, raw_req_headers: Dict.put(raw_req_headers, String.downcase(key), to_string(value)))
   end
 
   @doc """
   Deletes a request header.
   """
-  def delete_req_header(key, %Connection{raw_req_headers: raw_req_headers} = conn) do
-    %Connection{conn, raw_req_headers: Dict.delete(raw_req_headers, String.downcase(key))}
+  def delete_req_header(key, connection(raw_req_headers: raw_req_headers) = conn) do
+    connection(conn, raw_req_headers: Dict.delete(raw_req_headers, String.downcase(key)))
   end
 end
 
